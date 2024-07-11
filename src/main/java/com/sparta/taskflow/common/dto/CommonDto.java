3580f939f5c3409b9038896305e0786cd6bac235
HongYongGuen <163097270+HongYongGuen@users.noreply.github.com>
2024-07-11 12:30:16 +0900

    [#4] Feat : CommonDto 기능 구현
    - 공통 responseDto 기능할 CommonDto 구현

--- src/main/java/com/sparta/taskflow/common/dto/CommonDto.java
+++ src/main/java/com/sparta/taskflow/common/dto/CommonDto.java
@@ -1,4 +1,20 @@
 package com.sparta.taskflow.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class CommonDto<T> {
+    private final Integer status;
+    private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final T data;
+
+    @Builder
+    public CommonDto(Integer status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
 }
